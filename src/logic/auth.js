4f45d66f0ea07cf7ae8c21e6e5733ff79d6d71fd
PoulLorca <lorcapoul@gmail.com>
2023-01-05 23:40:20 -0300

    test

--- src/logic/auth.js
+++ src/logic/auth.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+/*import axios from "axios";
 import qs from "qs";
 
 const ENDPOINT_PATH = "http://www.apibagual.codigobagual.cl";
@@ -29,4 +29,51 @@ export default {
       return(error);
     });
   }
-    };
+    };*/
+
+export default{
+  register(){
+var https = require('follow-redirects').https;
+var fs = require('fs');
+
+var qs = require('querystring');
+
+var options = {
+  'method': 'POST',
+  'hostname': 'www.apibagual.codigobagual.cl',
+  'path': '/users?register=true&suffix=user',
+  'headers': {
+    'Authorization': 'M8kPAPvGZR2U9H35ZUxRW2S2xHD9J7',
+    'apikey': 'M8kPAPvGZR2U9H35ZUxRW2S2xHD9J7',
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  'maxRedirects': 20
+};
+
+var req = https.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function (chunk) {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+
+  res.on("error", function (error) {
+    console.error(error);
+  });
+});
+
+var postData = qs.stringify({
+  'email_user': 'usuario@correo.com',
+  'password_user': '123456'
+});
+
+console.log(req.write(postData));
+
+req.end();
+}
+}
